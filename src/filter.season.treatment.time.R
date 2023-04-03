bd4942d557b2dcc9519f0a96c4cfe9367e351750
onadeulofeu <85949575+onadeulofeu@users.noreply.github.com>
2023-04-03 15:12:05 +0200

    Add functions

---
+++ src/filter.season.treatment.time.R
@@ -0,0 +1,24 @@
+#' Filter season treatment and time in remei tibbles
+#'
+#' @param data a table with environmental data 
+#' @param treatment_col column where treatments are annotated
+#' @param treatment_keep treatments we want to keep one or more
+#' @param season_col column where seasons are annotated
+#' @param season_keep seasons we want to work with one or more
+#' @param time_col column where time id are annotated 
+#' @param time_keep times we want  to work with one or more
+#'
+#' @return a dataset filtered
+#' @export tidyverse
+#' @import dplyr
+#'
+#' @examples
+filter.season.treatment.time <- function(data, treatment_col, treatment_keep, season_col, season_keep, time_col, time_keep){
+  my_var1 <- rlang::enquo(treatment_col)
+  my_var2 <- rlang::enquo(season_col)
+  my_var3 <- rlang::enquo(time_col)
+  my_var4 <- rlang::enquo(treatment_keep)
+  data_filt <- data %>%
+    filter(!!my_var1 %in% treatment_keep & !!my_var2 %in% season_keep & !!my_var3 %in% time_keep)
+  return(data_filt)
+}
